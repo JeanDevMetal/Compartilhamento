90542deb23a476ee4a75087dd394b40041128c32
Idail Ferreira <neto_br_8@hotmail.com>
2021-06-03 21:39:35 -0400

    Modificado todo o formulario, adicionado for para mostrar o nome 10 vezes, e adicionado formulario para exibir informações logo abaixo, por idail.

--- Compartilhamento/Form1.Designer.cs
+++ Compartilhamento/Form1.Designer.cs
@@ -29,84 +29,161 @@ namespace Compartilhamento
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_inicial));
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.txt_nome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_nome_formulario = new System.Windows.Forms.TextBox();
+            this.txt_idade_formulario = new System.Windows.Forms.TextBox();
+            this.txt_cidade_formulario = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txt_informacao_nome_cadastro = new System.Windows.Forms.TextBox();
+            this.txt_informacao_idade_cadastro = new System.Windows.Forms.TextBox();
+            this.txt_informacao_cidade_cadastro = new System.Windows.Forms.TextBox();
+            this.lbl_informacao_cadastro = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 51);
+            this.button1.Location = new System.Drawing.Point(268, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(92, 23);
+            this.button1.Size = new System.Drawing.Size(158, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "SIM";
+            this.button1.Text = "Mostrar seu nome 10 vezes";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(110, 51);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(91, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "TALVEZ";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(207, 51);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(91, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "COMCERTEZA";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Pink;
-            this.label1.Location = new System.Drawing.Point(51, 9);
+            this.label1.Location = new System.Drawing.Point(12, 17);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(216, 33);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "IDAIL É GAY??";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Digite seu nome:";
             // 
-            // pictureBox1
+            // txt_nome
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(84, 95);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(142, 139);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 4;
-            this.pictureBox1.TabStop = false;
+            this.txt_nome.Location = new System.Drawing.Point(104, 12);
+            this.txt_nome.Name = "txt_nome";
+            this.txt_nome.Size = new System.Drawing.Size(158, 20);
+            this.txt_nome.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Digite seu nome:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(293, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Digite sua idade:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Digite sua cidade:";
+            // 
+            // txt_nome_formulario
+            // 
+            this.txt_nome_formulario.Location = new System.Drawing.Point(104, 50);
+            this.txt_nome_formulario.Name = "txt_nome_formulario";
+            this.txt_nome_formulario.Size = new System.Drawing.Size(183, 20);
+            this.txt_nome_formulario.TabIndex = 6;
+            // 
+            // txt_idade_formulario
+            // 
+            this.txt_idade_formulario.Location = new System.Drawing.Point(377, 50);
+            this.txt_idade_formulario.Name = "txt_idade_formulario";
+            this.txt_idade_formulario.Size = new System.Drawing.Size(49, 20);
+            this.txt_idade_formulario.TabIndex = 7;
+            // 
+            // txt_cidade_formulario
+            // 
+            this.txt_cidade_formulario.Location = new System.Drawing.Point(104, 85);
+            this.txt_cidade_formulario.Name = "txt_cidade_formulario";
+            this.txt_cidade_formulario.Size = new System.Drawing.Size(183, 20);
+            this.txt_cidade_formulario.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 123);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Cadastrar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // txt_informacao_nome_cadastro
+            // 
+            this.txt_informacao_nome_cadastro.Location = new System.Drawing.Point(17, 206);
+            this.txt_informacao_nome_cadastro.Name = "txt_informacao_nome_cadastro";
+            this.txt_informacao_nome_cadastro.Size = new System.Drawing.Size(272, 20);
+            this.txt_informacao_nome_cadastro.TabIndex = 10;
+            // 
+            // txt_informacao_idade_cadastro
+            // 
+            this.txt_informacao_idade_cadastro.Location = new System.Drawing.Point(298, 206);
+            this.txt_informacao_idade_cadastro.Name = "txt_informacao_idade_cadastro";
+            this.txt_informacao_idade_cadastro.Size = new System.Drawing.Size(42, 20);
+            this.txt_informacao_idade_cadastro.TabIndex = 11;
+            // 
+            // txt_informacao_cidade_cadastro
+            // 
+            this.txt_informacao_cidade_cadastro.Location = new System.Drawing.Point(17, 232);
+            this.txt_informacao_cidade_cadastro.Name = "txt_informacao_cidade_cadastro";
+            this.txt_informacao_cidade_cadastro.Size = new System.Drawing.Size(272, 20);
+            this.txt_informacao_cidade_cadastro.TabIndex = 12;
+            // 
+            // lbl_informacao_cadastro
+            // 
+            this.lbl_informacao_cadastro.AutoSize = true;
+            this.lbl_informacao_cadastro.Location = new System.Drawing.Point(83, 169);
+            this.lbl_informacao_cadastro.Name = "lbl_informacao_cadastro";
+            this.lbl_informacao_cadastro.Size = new System.Drawing.Size(35, 13);
+            this.lbl_informacao_cadastro.TabIndex = 13;
+            this.lbl_informacao_cadastro.Text = "label5";
             // 
             // frm_inicial
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.ClientSize = new System.Drawing.Size(309, 246);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button3);
+            this.ClientSize = new System.Drawing.Size(431, 259);
+            this.Controls.Add(this.lbl_informacao_cadastro);
+            this.Controls.Add(this.txt_informacao_cidade_cadastro);
+            this.Controls.Add(this.txt_informacao_idade_cadastro);
+            this.Controls.Add(this.txt_informacao_nome_cadastro);
             this.Controls.Add(this.button2);
+            this.Controls.Add(this.txt_cidade_formulario);
+            this.Controls.Add(this.txt_idade_formulario);
+            this.Controls.Add(this.txt_nome_formulario);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_nome);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.MaximizeBox = false;
             this.Name = "frm_inicial";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Formulario";
             this.Load += new System.EventHandler(this.frm_inicial_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -115,10 +192,19 @@ namespace Compartilhamento
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txt_nome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_nome_formulario;
+        private System.Windows.Forms.TextBox txt_idade_formulario;
+        private System.Windows.Forms.TextBox txt_cidade_formulario;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txt_informacao_nome_cadastro;
+        private System.Windows.Forms.TextBox txt_informacao_idade_cadastro;
+        private System.Windows.Forms.TextBox txt_informacao_cidade_cadastro;
+        private System.Windows.Forms.Label lbl_informacao_cadastro;
     }
 }
 
--- Compartilhamento/Form1.cs
+++ Compartilhamento/Form1.cs
@@ -12,6 +12,7 @@ namespace Compartilhamento
 {
     public partial class frm_inicial : Form
     {
+        int i = 0;
         public frm_inicial()
         {
             InitializeComponent();
@@ -34,7 +35,40 @@ namespace Compartilhamento
 
         private void frm_inicial_Load(object sender, EventArgs e)
         {
+            this.lbl_informacao_cadastro.Visible = false;
+            this.txt_informacao_nome_cadastro.Visible = false;
+            this.txt_informacao_cidade_cadastro.Visible = false;
+            this.txt_informacao_idade_cadastro.Visible = false;
+        }
 
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if(txt_nome.Text == "")
+            {
+                MessageBox.Show("Campo nome esta vazio, por favor preencha-o", "Informação");
+                txt_nome.Focus();
+            }else
+            {
+                for(i = 0;i < 10;i++)
+                {
+                    MessageBox.Show("O nome digitado foi:"+txt_nome.Text);
+                }
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            this.lbl_informacao_cadastro.Visible = true;
+            this.lbl_informacao_cadastro.Text = "===== CADASTRO REALIZADO COM SUCESSO =====";
+            
+            this.txt_informacao_nome_cadastro.Visible = true;
+            this.txt_informacao_nome_cadastro.Text = txt_nome_formulario.Text;
+
+            this.txt_informacao_cidade_cadastro.Visible = true;
+            this.txt_informacao_cidade_cadastro.Text = txt_cidade_formulario.Text;
+
+            this.txt_informacao_idade_cadastro.Visible = true;
+            this.txt_informacao_idade_cadastro.Text = txt_idade_formulario.Text;
         }
     }
 }
